5a79f8ac47aef4cd19a70658906ed93abcbd476a
Kyle <rhodestraveler@gmail.com>
2017-09-12 21:46:24 -0500

    checkpoint 3

--- script.jsx
+++ script.jsx
@@ -7,12 +7,47 @@ const styles = require('./style.css');
 class Fetch extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {};
+
+    this.topic;
+    this.fetchData = this.fetchData.bind(this)
+  }
+
+  fetchData () {
+    this.topic = this.props.value;
+    fetch(`https://en.wikipedia.org/w/api.php${topic}`).then((response) => {
+        response.json().then((data) => {
+          // console.log(data);
+          this.setState({
+
+          })
+        })
+      })
   }
 
   render() {
     return (
-      <h1>Address Book / Country Codes</h1>
-      // Your code here
+      <div>
+        <div>
+          <h2 value="Elephants">Info about: {this.props.value}</h2>
+          <ul>
+
+          </ul>
+        </div>
+        <div>
+          <h2 value="Leopards">Info about: {this.props.value}</h2>
+          <ul>
+
+          </ul>
+        </div>
+        <div>
+          <h2 value="Cobras">Info about: {this.props.value}</h2>
+          <ul>
+
+          </ul>
+        </div>
+      </div>
     );
   }
 }
