28f07a48c095d991dee8d69cc069b3c4d21f5b21
Kyle <rhodestraveler@gmail.com>
2018-03-17 22:25:29 -0500

    copied files from another project directory

--- script.jsx
+++ script.jsx
@@ -8,48 +8,63 @@ class Fetch extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      artistData: {},
+      searchArtist: '',
+      searchInput: ''
+    };
 
-    this.topic;
-    this.fetchData = this.fetchData.bind(this)
+    this.currentSearchQuery;
+    this.getSearchInput = this.getSearchInput.bind(this);
+    this.setSearchOutput = this.setSearchOutput.bind(this);
   }
 
-  fetchData () {
-    this.topic = this.props.value;
-    fetch(`https://en.wikipedia.org/w/api.php${topic}`).then((response) => {
-        response.json().then((data) => {
-          // console.log(data);
-          this.setState({
 
-          })
-        })
-      })
+  getSearchInput(event) {
+    const uriString = encodeURI(event.target.value);
+    this.currentSearchQuery = uriString;
+  }
+
+  setSearchOutput(event) {
+    event.preventDefault();
+
+    fetch(`https://itunes.apple.com/search?term=${this.currentSearchQuery}&limit=50`).then((response) => {
+        response.json().then((data) => {
+          this.setState({
+            artistData: data.results
+          });
+        });
+      });
   }
 
   render() {
+
+    let lis = [];
+
+    if(this.state.artistData.length >= 1) {
+      this.state.artistData.forEach((artist) =>{
+        lis.push(
+          <a href={artist.trackViewUrl} className='anc'>
+            <li key={artist.trackId}>{artist.trackName}  By: {artist.artistName}</li>
+          </a>
+        );
+      });
+    }
+
     return (
-      <div>
-        <div>
-          <h2 value="Elephants">Info about: {this.props.value}</h2>
-          <ul>
-
-          </ul>
-        </div>
-        <div>
-          <h2 value="Leopards">Info about: {this.props.value}</h2>
-          <ul>
-
-          </ul>
-        </div>
-        <div>
-          <h2 value="Cobras">Info about: {this.props.value}</h2>
-          <ul>
-
-          </ul>
-        </div>
+      <div className="react-wrapper">
+        <header>
+          <h1>Search iTunes for your favorite artist</h1>
+          <form>
+            <input type="text"  onChange={this.getSearchInput} />
+            <button onClick={this.setSearchOutput}>Search</button>
+          </form>
+        </header>
+        <ul>{lis}</ul>
       </div>
     );
   }
+
 }
 
 ReactDOM.render(<Fetch />, document.getElementById('fetch'));
